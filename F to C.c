5e73221ff6a74dd1a65fd78253c58a77faba93ac
Gaganapriyac <78781909+Gaganapriyac@users.noreply.github.com>
2021-02-10 20:40:32 +0530

    Update F to C.c

--- F to C.c
+++ F to C.c
@@ -5,7 +5,7 @@ int main()
     float C,F;
     printf("Enter the temperature in farhenheit\n");
     scanf("%f", &F);
-    c=(F-32)*5/9;
+    C=(F-32)*5/9;
     printf("The temperature in degree Celsius is %f",C);
     getch();
     return 0;
