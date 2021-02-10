33a4055c8b7530b05d361f463e93a117b7657474
Gaganapriyac <78781909+Gaganapriyac@users.noreply.github.com>
2021-02-10 21:45:25 +0530

    Create Roots of Quadratic function.c

---
+++ Roots of Quadratic function.c
@@ -0,0 +1,23 @@
+include<stdio.h>
+#include<math.h>
+int main()
+{ float a,b,c,root1,root2,
+    discriminant,realnum,imgnum;
+printf("\n Enter coefficients of a,b,c\n");
+scanf("%f %f %f",&a,&b,&c);
+discriminant=(b*b)-(4*a*c);
+if(discriminant>0)
+{ root1=(-b+sqrt(discriminant))/(2*a);
+  root1=(-b-sqrt(discriminant))/(2*a);
+}
+else if(discriminant==0)
+{ root1=root2=-b/(2*a);
+  printf("root1=root2=%f",root1);
+}
+else
+{realnum=(-b/(2*a));
+imgnum=sqrt(-discriminant)/(2*a);
+printf("root1=%f+%fi and root2=%f-%fi",realnum,imgnum);
+}
+return 0;
+}                 
