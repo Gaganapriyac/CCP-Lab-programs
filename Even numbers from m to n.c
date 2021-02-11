8e53fa62b876933dacd2dd33c8c359da2af6db5d
Gagana Priya C BMSCE <78781909+Gaganapriyac@users.noreply.github.com>
2021-02-11 19:45:04 +0530

    Create Even numbers from m to n.c

---
+++ Even numbers from m to n.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+
+#include<conio.h>
+
+void check(int, int);
+
+int main()
+
+{
+
+    int a,b;
+
+    printf ("\n Enter starting number:");
+
+    scanf ("%d", &a );
+
+    printf ("\n Enter ending number:");
+
+    scanf ("%d", &b );
+
+    check(a, b);
+
+    return 0;
+
+}
+
+void check(int m, int n)
+
+{
+
+    if (m>n)
+
+        printf ("\n Error: Please try again.");
+
+    else
+
+    {
+
+        int i;
+
+        printf ("\n Even numbers from %d to %d are:" ,m,n);
+
+        for(i=m; i<=n; i++)
+
+        {
+
+            if(i%2==0)
+
+            printf("%d \t", i);
+
+        }
+
+    }
+
+getch();
+
+}
